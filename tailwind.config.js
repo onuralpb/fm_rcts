c529420dda8953c7718b18b5ac5cb8420e16e9a5
Onur Alp Baklaci <obaklaci@yenibiris.com>
2023-08-03 19:02:05 +0300

    First Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        //Dark Blue (Dark Mode Elements)
+        darkBlue: "hsl(209, 23%, 22%)",
+
+        //Very Dark Blue (Dark Mode Background)
+        veryDarkBlue: "hsl(207, 26%, 17%)",
+
+        //Very Dark Blue (Light Mode Text)
+        veryDarkBlue2: "hsl(200, 15%, 8%)",
+
+        //Dark Gray (Light Mode Input)
+        darkGray: "hsl(0, 0%, 52%)",
+
+        //Very Light Gray (Light Mode Background)
+        veryLightGray: "hsl(0, 0%, 98%)",
       },
     },
   },
   plugins: [],
-}
+};
